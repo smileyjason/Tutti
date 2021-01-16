5976dd5e937308511b1bbf3366d74b72c72c2b58
smileyjason <jiajunzhang.jason@gmail.com>
2021-01-16 18:26:50 -0500

    MERGED ALL FILES AND FUNCTIONS

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QInputDialog, QDialog, QLineEdit, QMessageBox
 from PyQt5.QtGui import QIcon
-import speech_recognition
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QInputDialog, QDialog, QLineEdit, QMessageBox
+import speech_recognition   
 import csv
  
  
@@ -11,50 +15,62 @@ class MainWindow(QDialog):
         self.subWindow = None
         #Dialog.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         Dialog.setObjectName("Dialog")
-        Dialog.resize(574, 470)
+        Dialog.setGeometry(10,40,300,400)
+        Dialog.resize(300, 400) #width and height
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(Dialog)
-        self.verticalLayout_2.setContentsMargins(11, 11, 11, 11)
-        self.verticalLayout_2.setSpacing(6)
+        self.verticalLayout_2.setContentsMargins(5, 5, 5, 5)
+        self.verticalLayout_2.setSpacing(5)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setContentsMargins(11, 11, 11, 11)
-        self.horizontalLayout.setSpacing(6)
+        self.horizontalLayout.setContentsMargins(5, 5, 5, 5)
+        self.horizontalLayout.setSpacing(5)
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.listWidget = QtWidgets.QListWidget(Dialog)
         self.listWidget.setObjectName("listWidget")
         self.horizontalLayout.addWidget(self.listWidget)
         self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.verticalLayout.setContentsMargins(11, 11, 11, 11)
-        self.verticalLayout.setSpacing(6)
+        self.verticalLayout.setContentsMargins(5, 5, 5, 5)
+        self.verticalLayout.setSpacing(5)
         self.verticalLayout.setObjectName("verticalLayout")
+        
+        #add button
         self.pushButton_add = QtWidgets.QPushButton(Dialog)
         self.pushButton_add.setObjectName("pushButton_add")
-        
         self.verticalLayout.addWidget(self.pushButton_add)
+        
+        #edit button
         self.pushButton_edit = QtWidgets.QPushButton(Dialog)
         self.pushButton_edit.setObjectName("pushButton_edit")
-        
         self.verticalLayout.addWidget(self.pushButton_edit)
+
+        #remove button
         self.pushButton_remove = QtWidgets.QPushButton(Dialog)
         self.pushButton_remove.setObjectName("pushButton_remove")
-        
         self.verticalLayout.addWidget(self.pushButton_remove)
+
+        ##this is the button to move up
         self.pushButton_up = QtWidgets.QPushButton(Dialog)
         self.pushButton_up.setObjectName("pushButton_up")
         self.verticalLayout.addWidget(self.pushButton_up)
+        
+        ##this is the button to move down
         self.pushButton_down = QtWidgets.QPushButton(Dialog)
         self.pushButton_down.setObjectName("pushButton_down")
         self.verticalLayout.addWidget(self.pushButton_down)
+
+        ##this is the button to sort
         self.pushButton_sort = QtWidgets.QPushButton(Dialog)
         self.pushButton_sort.setObjectName("pushButton_sort")
         self.verticalLayout.addWidget(self.pushButton_sort)
-        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout.addItem(spacerItem)
         
-        #Close button
-        self.pushButton_close = QtWidgets.QPushButton(Dialog)
-        self.pushButton_close.setObjectName("pushButton_close")
-        self.verticalLayout.addWidget(self.pushButton_close)
+        #spacer btwn types of buttons
+        spacerItem = QtWidgets.QSpacerItem(5, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem)
+
+         #Q2T button
+        self.pushButton_Q2T = QtWidgets.QPushButton(Dialog)
+        self.pushButton_Q2T.setObjectName("pushButton_Q2T")
+        self.verticalLayout.addWidget(self.pushButton_Q2T)
 
         #Open csv button
 
@@ -67,35 +83,43 @@ class MainWindow(QDialog):
         self.pushButton_save.setObjectName("pushButton_save")
         self.verticalLayout.addWidget(self.pushButton_save)
     
-        
+        #hover/hide button (FIX)
+        self.pushButton_hide = QtWidgets.QPushButton(Dialog)
+        self.pushButton_hide.setObjectName("pushButton_hide")
+        self.verticalLayout.addWidget(self.pushButton_hide)
 
-        self.hoverButton = QtWidgets.QPushButton(Dialog)
-        self.hoverButton.setObjectName("hoverButton")
-        self.verticalLayout.addWidget(self.hoverButton)
-        
+    
+        #push to talk button
         self.voiceButton = QtWidgets.QPushButton(Dialog)
         self.voiceButton.setObjectName("voiceButton")
         self.verticalLayout.addWidget(self.voiceButton)
 
         self.horizontalLayout.addLayout(self.verticalLayout)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        ##this is the quit button
+        self.pushButton_quit = QtWidgets.QPushButton(Dialog)
+        self.pushButton_quit.setObjectName("pushButton_quit")
+        self.verticalLayout.addWidget(self.pushButton_quit)
  
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
+        
+        #directing the buttons to different functions when clicked
         self.pushButton_add.clicked.connect(self.add)
         self.pushButton_edit.clicked.connect(self.edit)
         self.pushButton_remove.clicked.connect(self.remove)
         self.pushButton_up.clicked.connect(self.up)
         self.pushButton_down.clicked.connect(self.down)
         self.pushButton_sort.clicked.connect(self.sort)
-        self.pushButton_close.clicked.connect(self.close)
-        self.hoverButton.installEventFilter(self)
+        self.pushButton_hide.clicked.connect(self.hide)
         self.voiceButton.clicked.connect(self.voicerecognition)
         self.pushButton_open.clicked.connect(self.opencsv)
         self.pushButton_save.clicked.connect(self.saveFileDialog)
+        self.pushButton_quit.clicked.connect(self.quit)
  
         self.TASK()
-
+        #Dialog.show()
         
  
     def retranslateUi(self, Dialog):
@@ -107,16 +131,15 @@ class MainWindow(QDialog):
         self.pushButton_remove.setText(_translate("Dialog", "Remove"))
         self.pushButton_up.setText(_translate("Dialog", "Up"))
         self.pushButton_down.setText(_translate("Dialog", "Down"))
-        self.pushButton_sort.setText(_translate("Dialog", "Sort by Date"))
-        self.pushButton_close.setText(_translate("Dialog", "Close"))
-        self.hoverButton.setText(_translate("Dialog", "Hover"))
+        self.pushButton_sort.setText(_translate("Dialog", "Sort"))
+        self.pushButton_hide.setText(_translate("Dialog", "Hide"))
+        self.pushButton_Q2T.setText(_translate("Dialog", "Q2T"))
+        self.pushButton_quit.setText(_translate("Dialog", "Quit"))
         self.voiceButton.setText(_translate("Dialog", "Speech to Text"))
         self.pushButton_open.setText(_translate("Dialog", "Open"))
         self.pushButton_save.setText(_translate("Dialog", "Save"))
  
- 
- 
- 
+
     def TASK(self):
  
  
@@ -145,8 +168,8 @@ class MainWindow(QDialog):
                 date = date + "."
             
             self.tasks.update({date : text})
- 
- 
+        
+
     def edit(self):
         row = self.listWidget.currentRow()
         item = self.listWidget.item(row)
@@ -167,10 +190,10 @@ class MainWindow(QDialog):
                 else:
                     item.setText(string + "     " + deadline)
                     self.tasks.update({deadline : string})
-                
- 
+    
  
     def remove(self):
+        
         row = self.listWidget.currentRow()
         item = self.listWidget.item(row)
  
@@ -210,7 +233,7 @@ class MainWindow(QDialog):
             self.listWidget.insertItem(row + 1, item)
             self.listWidget.setCurrentItem(item)
  
-
+ 
     def sort(self):
         row = len(self.tasks.items())
         sorteditems = sorted(self.tasks.items())
@@ -225,10 +248,19 @@ class MainWindow(QDialog):
             self.listWidget.insertItem(row, nodead[1])
  
  
-    def close(self):
+    def quit(self):
         quit()
  
- 
+    def hide(self):
+        #self.setWindowOpacity(0.)
+        Dialog.close()
+        self.callAnotherQMainWindow()
+        return True
+        
+    #def closewin(self):
+        #self.close()
+
+    '''   
     def eventFilter(self, object, event):
         if event.type() == QtCore.QEvent.Enter and self.subWindow == None:
             self.subWindow = True
@@ -239,10 +271,10 @@ class MainWindow(QDialog):
             self.subWindow = None
             return False
         return False
-    
+    '''
     def callAnotherQMainWindow(self):
         self.subWindow = ChildWindow(self)
-        self.setWindowOpacity(0.)
+        #self.setWindowOpacity(0.)
         self.newWindow = True
         return self.subWindow
 
@@ -309,7 +341,7 @@ class MainWindow(QDialog):
 
         except Exception as ex:
             print("Sorry. Could not understand.")
-            
+
     def speechtotext(self):
         recognizer = speech_recognition.Recognizer()
         with speech_recognition.Microphone() as src:
@@ -320,26 +352,22 @@ class MainWindow(QDialog):
             print(text)       
             return text
 
-    
-        
-
     def saveFileDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"Save File","","CSV Files (*.csv)")
-        
-        with open(fileName, "w") as fileInput:
-            fileWriter = csv.writer(fileInput, delimiter = ",", quotechar = '"', quoting = csv.QUOTE_MINIMAL) 
-            for row in self.tasks.items():
-                if row:
-                    if row[0] == row[1]:
-                        fileWriter.writerow([row[1]])
-                    else:
-                        fileWriter.writerow([row[1], row[0]])
-        
+            options = QFileDialog.Options()
+            options |= QFileDialog.DontUseNativeDialog
+            fileName, _ = QFileDialog.getSaveFileName(self,"Save File","","CSV Files (*.csv)")
+            
+            with open(fileName, "w") as fileInput:
+                fileWriter = csv.writer(fileInput, delimiter = ",", quotechar = '"', quoting = csv.QUOTE_MINIMAL) 
+                for row in self.tasks.items():
+                    if row:
+                        if row[0] == row[1]:
+                            fileWriter.writerow([row[1]])
+                        else:
+                            fileWriter.writerow([row[1], row[0]])
+            
 
-        #### MAKE NEW CSV WITH THE CURRENT DICT
-
+            #### MAKE NEW CSV WITH THE CURRENT DICT
     def opencsv(self):
         fileName, _ = QFileDialog.getOpenFileName(self, "Open File","","CSV Files (*.csv)")
         with open(fileName, "r") as fileInput:
@@ -367,17 +395,53 @@ class MainWindow(QDialog):
                             self.tasks.update({date : row[0]})
             self.sort()
                 
+
     
 class ChildWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent = None):
-        super().__init__(parent)
-        self.parent = parent
+    def __init__(self,children = None):
+        super().__init__(children)
+        self.children = children
+        self.subWindow = None
+        self.setGeometry(200,400,100,20)
+        #self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        #self.setFixedSize(100, 50)
+       
+
+       #makes the button
+        self.button = QtWidgets.QPushButton("Hover", self)
+        self.button.setGeometry(1,1,50,20) # x,y, width, height
+        self.button.installEventFilter(self)
+        sg = QDesktopWidget().availableGeometry() # height of the screen
+        self.move(0,(sg.height() - self.geometry().height())/2)
         self.show()
+
+    
+    def eventFilter(self, object, event):
+        if event.type() == QtCore.QEvent.Enter and self.subWindow == None:
+            self.subWindow = True
+            self.callAnotherQMainWindow()
+
+            #self.close()
+          
+            return True    
+        #elif event.type() == QtCore.QEvent.Leave and self.subWindow != None:
+            #self.subWindow.close()
+            #self.subWindow = None
+            #return False
+            
+        return False
+
+    def callAnotherQMainWindow(self):
         
+        self.setWindowOpacity(0.)
+        
+        Dialog.show()
+        
+       
 
     def closeEvent(self, QCloseEvent):
-        self.parent.setWindowOpacity(1.)
- 
+        #self.children.setWindowOpacity(1.)
+        pass
  
  
 if __name__ == "__main__":
